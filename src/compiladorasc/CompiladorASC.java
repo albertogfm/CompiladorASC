2cb374fbefad5c4c08b40060f2ffe4fc1fd5a4ab
Alberto <albertogarciafigueroam@gmail.com>
2020-11-13 20:01:51 -0600

    grafico

--- src/compiladorasc/CompiladorASC.java
+++ src/compiladorasc/CompiladorASC.java
@@ -10,6 +10,7 @@ package compiladorasc;
 import regex.*;
 import filemanagment.*;
 import java.util.ArrayList;
+import graficas.*;
 
 /**
  *
@@ -21,13 +22,9 @@ public class CompiladorASC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FileMan trataFiles = new FileMan();
-        //System.out.println(trataFiles.readOpcodes("adca","INDX"));
-        trataFiles.leerArchivo("ERROR");
-        for (int i = 0; i < 500; i++) {
-            System.out.println(trataFiles.lineasArchivoASC.get(i)); 
-        }
-      
+        Inicio ini = new Inicio();
+        ini.setVisible(true);
+     
     }
     
 }
